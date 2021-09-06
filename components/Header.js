cc22ba2ee0bea5e3bcb7f1782a943200c00285e4
Mathieu Fiani <mathieu.fiani2@gmail.com>
2021-09-07 01:48:54 +0200

    added first component

--- components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <Header className="">
-      <h1>This is the Header</h1>
-    </Header>
-  );
-}
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="">
+      <h1>This is the Header</h1>
+    </header>
+  );
+}
+
+export default Header;
