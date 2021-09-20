e71a1ed9b4e712f61a22a5a3f1a0de25a7b41982
mathieufiani <mathieu.fiani@gmail.com>
2021-09-20 19:08:20 +0200

    added margin 5 to thumbnail component

--- components/common/Thumbnail.jsx
+++ components/common/Thumbnail.jsx
@@ -6,7 +6,7 @@ import {forwardRef} from "react";
 const Thumbnail = forwardRef(({result}, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original"
     return (
-        <div ref={ref} className={'p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'}>
+        <div ref={ref} className={'p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 m-5'}>
             <Image layout={'responsive'}
                    src={
                        `${BASE_URL}${result.backdrop_path || result.poster_path}`
